a786963565c4cb6ffb9027a643a583d1ae7eb29a
Pratik Raj <Rajpratik71@gmail.com>
2019-05-19 02:14:23 +0530

    Update msfweb.sh

--- msfweb.sh
+++ msfweb.sh
@@ -2,7 +2,7 @@ sudo -H apt-get --allow-unauthenticated --yes update
 sudo -H apt-get --allow-unauthenticated --yes upgrade -y
 sudo -H apt-get --allow-unauthenticated --yes install golang git build-essential patch ruby ruby-dev libsqlite3-dev -y 
 sudo -H apt-get --allow-unauthenticated --yes install libpcap-dev libpq-dev zlib1g-dev libz-dev liblzma-dev -y
-gem install bundler -v 1.16.1
+sudo -H gem install bundler -v 1.16.1
 cd ~ && git clone https://github.com/rapid7/metasploit-framework.git
 cd  /home/$USER/metasploit-framework && bundle install
 go get github.com/yudai/gotty
